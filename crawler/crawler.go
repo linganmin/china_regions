f915e944c746588fc64ca1e3645696c225548e67
linganmin <saboran@163.com>
2023-03-02 13:39:54 +0800

    crawler

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -5,39 +5,144 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
+	"regexp"
+	"strings"
+	"time"
 
 	"github.com/linganmin/zaplog"
 )
+
+const ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+
+const BaseUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2022/"
 
 func fetch(ctx context.Context, url string) (string, error) {
 	logger := zaplog.FromContext(ctx)
 	logger.Debugf("fetch url:%+v", url)
 
+	// 页面有并发限制，随机暂停
+	time.Sleep(time.Millisecond * time.Duration(rand.Int31n(100)+40))
+
 	cli := &http.Client{}
 
-	req, _ := http.NewRequest("GET", url, nil)
+	cli.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
 
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", ua)
 
 	resp, err := cli.Do(req)
-
 	if err != nil {
-		logger.Errorf("cli.Do error:%+v", err)
+		logger.Errorf("cli.Do resp:%v error:%+v", resp, err)
+
 		return "", err
 	}
+
 	if resp.StatusCode != http.StatusOK {
 		return "", errors.New(fmt.Sprintf("Http resp status not ok:%+v", resp))
 	}
 
 	defer resp.Body.Close()
-
 	body, err := io.ReadAll(resp.Body)
 
 	if err != nil {
-		logger.Errorf("io.ReadAll error:%+v", err)
+		logger.Errorf("io.ReadAll body:%v error:%+v", string(body), err)
 		return "", err
 	}
 
 	return string(body), nil
+}
+
+type regionPage struct {
+	Code  string `json:"code"`
+	Name  string `json:"name"`
+	Url   string `json:"url"`
+	Level int    `json:"level"`
+}
+
+type Level int
+
+const (
+	LevelProvince Level = iota + 1
+	LevelCity
+	LevelCounty
+	LevelTown
+	LevelVillage
+)
+
+func FetchProvincePages(ctx context.Context, url string) []regionPage {
+	logger := zaplog.FromContext(ctx)
+	content, err := fetch(ctx, url)
+
+	if err != nil {
+		logger.Errorf("FetchProvincePages error:%+v", err)
+		return nil
+	}
+	rg := regexp.MustCompile("href=\"(\\d*\\.html)\">([\u4e00-\u9fa5]*)")
+
+	items := rg.FindAllStringSubmatch(content, -1)
+
+	var list []regionPage
+
+	for _, item := range items {
+
+		if err != nil {
+			logger.Errorf("strconv.Atoi error:%+v", err)
+			continue
+		}
+
+		list = append(list, regionPage{
+			Code:  strings.Replace(item[1], ".html", "", -1) + strings.Repeat("0", 10),
+			Url:   BaseUrl + item[1],
+			Name:  item[2],
+			Level: 1,
+		})
+	}
+	return list
+}
+
+func FetchPages(ctx context.Context, url string, level Level) []regionPage {
+	if url == BaseUrl {
+		return nil
+	}
+	logger := zaplog.FromContext(ctx)
+	content, err := fetch(ctx, url)
+
+	if err != nil {
+		logger.Errorf("FetchCityPages error:%+v", err)
+		return nil
+	}
+
+	content = strings.Replace(content, "\r\n", "", -1)
+
+	rg := regexp.MustCompile("(\\d+/\\d+\\.html)*[\">td<]*(\\d{12})[<A-z/> =\\d.\"]*([\u4e00-\u9fa5]{0,30})")
+	items := rg.FindAllStringSubmatch(content, -1)
+
+	var list []regionPage
+
+	for _, item := range items {
+
+		url := BaseUrl
+
+		if item[1] != "" {
+			switch level {
+			case LevelCity:
+				url = BaseUrl + item[1]
+			case LevelCounty:
+				url = BaseUrl + item[2][:2] + "/" + item[1]
+			case LevelTown:
+				url = BaseUrl + item[2][:2] + "/" + item[2][2:4] + "/" + item[1]
+			}
+		}
+		list = append(list, regionPage{
+			Code:  item[2],
+			Url:   url,
+			Name:  item[3],
+			Level: int(level),
+		})
+	}
+	return list
 }
--- main.go
+++ main.go
@@ -1,29 +1,96 @@
 package main
 
 import (
+	"bufio"
 	"context"
-	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/linganmin/zaplog"
+
+	"github.com/linganmin/china_regions/crawler"
 )
 
 func main() {
 	ctx := context.WithValue(context.Background(), "request_id", uuid.NewString())
-
 	logger := zaplog.FromContext(ctx)
+	rand.Seed(time.Now().UnixNano())
 
-	logger.Errorf("this is a test error msg error: %+v", errors.New("name filed is required"))
-	logger.Infof("this is a test info msg name: %+v", "saboran")
-
-	type user struct {
-		Name string
-		Age  int
-		Addr string
+	wd, err := os.Getwd()
+	if err != nil {
+		logger.Errorf("get wd error:%+v", err)
+		return
 	}
-	logger.Debugf("this is a test debug msg user: %+v", user{
-		Name: "saboran",
-		Age:  18,
-		Addr: "The People's Republic of China",
-	})
+	filename := wd + "/" + time.Now().Format("2006-01-02") + ".sql"
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0666)
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+
+	// 所有省份
+	provinces := crawler.FetchProvincePages(ctx, crawler.BaseUrl)
+	for _, province := range provinces {
+		_, err = writer.WriteString(fmt.Sprintf("------------------ %s\n", province.Name))
+		_, err = writer.WriteString(fmt.Sprintf("insert into regions (code,name, level, p_code) values (%v,'%v',%v,%v);\n", province.Code, province.Name, province.Level, 0))
+
+		if err != nil {
+			logger.Errorf("write file error:%+v", err)
+			return
+		}
+		// 地级市
+		cities := crawler.FetchPages(ctx, province.Url, crawler.LevelCity)
+		for _, city := range cities {
+			_, err = writer.WriteString(fmt.Sprintf("--- %s\n", city.Name))
+
+			_, err = writer.WriteString(fmt.Sprintf(fmt.Sprintf("insert into regions (code,name, level, p_code) values (%v,'%v',%v,%v);\n", city.Code, city.Name, city.Level, province.Code)))
+			if err != nil {
+				logger.Errorf("write file error:%+v", err)
+				return
+			}
+
+			// 县/区
+			counties := crawler.FetchPages(ctx, city.Url, crawler.LevelCounty)
+			for _, county := range counties {
+				_, err = writer.WriteString(fmt.Sprintf("--- %s\n", county.Name))
+
+				_, err = writer.WriteString(fmt.Sprintf(fmt.Sprintf("insert into regions (code,name, level, p_code) values (%v,'%v',%v,%v);\n", county.Code, county.Name, county.Level, city.Code)))
+				if err != nil {
+					logger.Errorf("write file error:%+v", err)
+					return
+				}
+
+				// 镇/街道
+				towns := crawler.FetchPages(ctx, county.Url, crawler.LevelTown)
+				for _, town := range towns {
+					_, err = writer.WriteString(fmt.Sprintf("--- %s\n", town.Name))
+					_, err = writer.WriteString(fmt.Sprintf(fmt.Sprintf("insert into regions (code,name, level, p_code) values (%v,'%v',%v,%v);\n", town.Code, town.Name, town.Level, county.Code)))
+					if err != nil {
+						logger.Errorf("write file error:%+v", err)
+						return
+					}
+
+					// 村/居委会
+					villages := crawler.FetchPages(ctx, town.Url, crawler.LevelVillage)
+					for _, village := range villages {
+						_, err = writer.WriteString(fmt.Sprintf(fmt.Sprintf("insert into regions (code,name, level, p_code) values (%v,'%v',%v,%v);\n", village.Code, village.Name, village.Level, town.Code)))
+						if err != nil {
+							logger.Errorf("write file error:%+v", err)
+							return
+						}
+					}
+				}
+
+			}
+
+		}
+
+	}
+
+	err = writer.Flush()
+
+	if err != nil {
+		logger.Errorf("writer.Flush error:%+v", err)
+	}
 }
